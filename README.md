92f283d02a5ca3f9b78141b66d29b41261ee34ae
Elliot Forbes <emforce@live.com>
2022-03-08 19:57:46 +0000

    Version 0.0.7

---
+++ README.md
@@ -0,0 +1,19 @@
+Remotion Components
+====================
+
+### Components
+
+```ts
+import {Logo} from '@tutorialedge/remotion-components';
+
+<Logo />
+```
+
+### Publishing New Versions
+
+* Update `package.json` to the new version number
+* Run the publish task:
+
+```bash
+$ task publish
+```
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useCurrentFrame, Img } from "remotion";
+import { FC } from 'react';
+import logo from "./../../assets/logo.svg"
 
 export interface LogoProps {
 }
 
-const Logo = (props: LogoProps) => {
-  return <Logo />;
+const Logo: FC = () => {
+    const frame = useCurrentFrame();
+    const opacity = frame >= 20 ? 1 : frame / 20;
+    return (
+        <Img style={{
+            width: "100px",
+            height: "100px",
+            margin: "20px",
+            bottom: "20px",
+            opacity: opacity,
+            right: "0px",
+            position: "absolute"
+        }} src={logo} />
+    );
 };
 
 export default Logo;
